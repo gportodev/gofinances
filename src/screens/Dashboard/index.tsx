5012bdafafb2a820c03ea8306a6fe17cc04b595c
gamonpo <gamonpo@outlook.com>
2021-06-29 16:37:28 -0300

    Create List

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import HighlightCard from '../../components/HighlightCard';
-import TransactionCard from '../../components/TransactionCard';
+import TransactionCard, {
+  TransactionCardProps,
+} from '../../components/TransactionCard';
 
 import {
   Container,
@@ -14,58 +16,105 @@ import {
   HighlightCards,
   Transactions,
   Title,
+  TransactionList,
 } from './styles';
 
-const Dashboard: React.FC = () => (
-  <Container>
-    <Header>
-      <UserWrapper>
-        <UserInfo>
-          <Photo
-            source={{
-              uri: 'https://avatars.githubusercontent.com/u/34238796?v=4',
-            }}
-          />
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
 
-          <User>
-            <Text>Olá,</Text>
-            <Text>Gabriel</Text>
-          </User>
-        </UserInfo>
+const Dashboard: React.FC = () => {
+  const data: DataListProps[] = [
+    {
+      id: '1',
+      type: 'positive',
+      title: 'Desenvolvimento de site',
+      amount: 'R$ 12.000,00',
+      category: {
+        name: 'Vendas',
+        icon: 'dollar-sign',
+      },
+      date: '13/04/2020',
+    },
+    {
+      id: '2',
+      type: 'negative',
+      title: 'Hamburgueria Pizzy',
+      amount: 'R$ 59,00',
+      category: {
+        name: 'Alimentação',
+        icon: 'coffee',
+      },
+      date: '10/04/2020',
+    },
+    {
+      id: '3',
+      type: 'negative',
+      title: 'Aluguel do apartamento',
+      amount: 'R$ 1.200,00',
+      category: {
+        name: 'Casa',
+        icon: 'shopping-bag',
+      },
+      date: '10/04/2020',
+    },
+  ];
 
-        <Icon name="power" />
-      </UserWrapper>
-    </Header>
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/34238796?v=4',
+              }}
+            />
 
-    <HighlightCards>
-      <HighlightCard
-        type="up"
-        title="Entradas"
-        amount="R$ 17.400,00"
-        lastTransatcion="Última entrada dia 13 de abril"
-      />
+            <User>
+              <Text>Olá,</Text>
+              <Text>Gabriel</Text>
+            </User>
+          </UserInfo>
 
-      <HighlightCard
-        type="down"
-        title="Saídas"
-        amount="R$ 1.259,00"
-        lastTransatcion="Última saída dia 03 de abril"
-      />
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
 
-      <HighlightCard
-        type="total"
-        title="Total"
-        amount="R$ 16.141,00"
-        lastTransatcion="01 à 16 de abril"
-      />
-    </HighlightCards>
+      <HighlightCards>
+        <HighlightCard
+          type="up"
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransatcion="Última entrada dia 13 de abril"
+        />
 
-    <Transactions>
-      <Title>Listagem</Title>
+        <HighlightCard
+          type="down"
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransatcion="Última saída dia 03 de abril"
+        />
 
-      <TransactionCard />
-    </Transactions>
-  </Container>
-);
+        <HighlightCard
+          type="total"
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransatcion="01 à 16 de abril"
+        />
+      </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+
+        <TransactionList
+          data={data}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+        />
+      </Transactions>
+    </Container>
+  );
+};
 
 export default Dashboard;
