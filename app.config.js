63c7da93d3bea2070effb43c0133ba277c3096d7
gportodev <gportodev@gmail.com>
2025-05-05 11:48:37 -0300

    chore: add eas update

--- app.config.js
+++ app.config.js
@@ -9,10 +9,16 @@ module.exports = {
       resizeMode: 'contain',
       backgroundColor: '#5636D3',
     },
+    updates: {
+      url: "https://u.expo.dev/86270e17-8f77-4832-a5fa-39dd5abea03d"
+    },
     assetBundlePatterns: ["**/*"],
     ios: {
       bundleIdentifier: "com.gofinance",
       buildNumber: "1.0.2",
+      runtimeVersion: {
+        policy: "appVersion"
+      }
     },
     android: {
       package: "com.gofinance",
@@ -21,6 +27,7 @@ module.exports = {
         foregroundImage: './assets/adaptive-icon.png',
         backgroundColor: '#5636D3',
       },
+      runtimeVersion: "1.0.0"
     },
     plugins: ['expo-font', '@react-native-google-signin/google-signin'],
     scheme: "gofinance",
